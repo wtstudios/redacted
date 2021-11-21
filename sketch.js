c956cd91c42b02c99c00b4479c868b4b26feb6ec
test <77040285+test1374@users.noreply.github.com>
2021-11-20 19:07:30 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let logo, survivr1, menurect, battleicon, roboto, menugun, backgimg;
+let logo, survivr1, menurect, battleicon, roboto, menugun, backgimg, bush1, bush2, bush3, tree1, tree2, tree3, acacia;
 function preload() {
   logo = loadImage('Survivlogo.png');
   survivr1 = loadImage('https://surviv.io/img/gui/loadout-player-icon.svg');
@@ -7,12 +7,20 @@ function preload() {
   roboto = loadFont('RobotoCondensed-Bold.ttf');
   menugun = loadImage('project.png');
   backgimg = loadImage('https://surviv.io/img/main_splash.jpg');
+  bush1 = loadImage('https://static.wikia.nocookie.net/survivio/images/6/69/Map-bush-01.png/revision/latest/scale-to-width-down/600?cb=20180622141749');
+  bush2 = loadImage('https://static.wikia.nocookie.net/survivio/images/c/c6/Map-bush-07-0.png/revision/latest/scale-to-width-down/180?cb=20181203124424');
+  bush3 = loadImage('https://static.wikia.nocookie.net/survivio/images/4/49/Map-bush-01sv.png/revision/latest/scale-to-width-down/700?cb=20190912121630');
+  acacia = loadImage('https://static.wikia.nocookie.net/survivio/images/0/0e/Obstacle-tree-12.png/revision/latest/scale-to-width-down/700?cb=20190901100319');
+  tree1 = loadImage('https://static.wikia.nocookie.net/survivio/images/2/25/Obstacle-tree-01.png/revision/latest/scale-to-width-down/700?cb=20191002105944');
+  tree2 = loadImage('https://static.wikia.nocookie.net/survivio/images/1/18/Obstacle-tree-03sv.png/revision/latest/scale-to-width-down/699?cb=20200220085833');
+  tree3 = loadImage('https://static.wikia.nocookie.net/survivio/images/1/11/Woods_Red_Tree.png/revision/latest/scale-to-width-down/700?cb=20201219170606');
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  cursor('default');
   noStroke();
   background(0);
   imageMode(CENTER);
@@ -31,14 +39,26 @@ function draw() {
   fill(131, 150, 80);
   rect(width / 3.2, height / 1.32 + 3, width / 9, width / 25, 5);
   fill(131, 175, 80);
+  if(mouseX >= width / 3.2 - width / 18 && mouseX <= width / 3.2 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
+    cursor('not-allowed');
+    fill(131, 150, 80);
+  }
   rect(width / 3.2, height / 1.32, width / 9, width / 25, 5);
   fill(131, 150, 80);
   rect(width / 2, height / 1.32 + 3, width / 9, width / 25, 5);
   fill(131, 175, 80);
+  if(mouseX >= width / 2 - width / 18 && mouseX <= width / 2 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
+    cursor('not-allowed');
+    fill(131, 150, 80);
+  }
   rect(width / 2, height / 1.32, width / 9, width / 25, 5);
   fill(131, 150, 80);
   rect(width / 1.5, height / 1.32 + 3, width / 9, width / 25, 5);
   fill(131, 175, 80);
+  if(mouseX >= width / 1.5 - width / 18 && mouseX <= width / 1.5 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
+    cursor('not-allowed');
+    fill(131, 150, 80);
+  }
   rect(width / 1.5, height / 1.32, width / 9, width / 25, 5);
   textSize(width / 40);
   fill(255);
@@ -47,14 +67,4 @@ function draw() {
   text('Loadout', width / 1.5, height / 1.3);
   width = windowWidth;
   height = windowHeight;
-  cursor('default');
-  if(mouseX >= width / 3.2 - width / 18 && mouseX <= width / 3.2 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
-    cursor('not-allowed');
-  }
-  if(mouseX >= width / 2 - width / 18 && mouseX <= width / 2 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
-    cursor('not-allowed');
-  }
-  if(mouseX >= width / 1.5 - width / 18 && mouseX <= width / 1.5 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
-    cursor('not-allowed');
-  }
 }
