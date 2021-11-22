d006db14e88cbe095aa5699587daced50065bbf1
test <77040285+test1374@users.noreply.github.com>
2021-11-21 21:46:13 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-let status, logo, survivr1, menurect, battleicon, roboto,jura, menugun, backgimg, bush1, bush2, bush3, tree1, tree2, tree3, acacia, stone, barrel, table1, table2, table3, mp5loot, mp5world, m1a1loot, m1a1world;
+let status, logo, survivr1, menurect, battleicon, roboto,jura, menugun, backgimg, bush1, bush2, bush3, tree1, tree2, tree3, acacia, stone, barrel, table1, table2, table3, visorred, visorblue, helmet, forestcamobase, snowcamobase, urbancamobase, desertcamo, mp5loot, mp5world, m1a1loot, m1a1world, auth;
 function preload() {
-  logo = loadImage('Survivlogo.png');
+  logo = loadImage('surviv_logo_full.png');
   survivr1 = loadImage('https://surviv.io/img/gui/loadout-player-icon.svg');
   menurect = loadImage('rect.png');
   battleicon = loadImage('https://surviv.io/img/gui/emote.svg');
@@ -17,10 +17,16 @@ function preload() {
   tree3 = loadImage('https://static.wikia.nocookie.net/survivio/images/1/11/Woods_Red_Tree.png/revision/latest/scale-to-width-down/700?cb=20201219170606');
   stone = loadImage('https://static.wikia.nocookie.net/survivio/images/c/c1/Map-stone-01.png/revision/latest/scale-to-width-down/700?cb=20180327172808');
   barrel = loadImage('https://static.wikia.nocookie.net/survivio/images/3/39/Map-barrel-01.png/revision/latest/scale-to-width-down/700?cb=20180327172838');
+  visorred = loadImage('https://static.wikia.nocookie.net/survivio/images/9/92/Player-visor-demo.img.png/revision/latest/scale-to-width-down/176?cb=20191201230453');
+  visorblue = loadImage('https://static.wikia.nocookie.net/survivio/images/7/70/Player-visor-sniper.img.png/revision/latest/scale-to-width-down/176?cb=20191201230455');
   mp5loot = loadImage('https://static.wikia.nocookie.net/survivio/images/3/3f/Loot-weapon-mp5.png/revision/latest/scale-to-width-down/700?cb=20180128214558');
   mp5world = loadImage('https://static.wikia.nocookie.net/survivio/images/f/f1/Gun-med-01.img.png/revision/latest/scale-to-width-down/32?cb=20190830235718');
   m1a1loot = loadImage('https://static.wikia.nocookie.net/survivio/images/4/42/Loot-weapon-m1a1.png/revision/latest/scale-to-width-down/700?cb=20181110003201');
   m1a1world = loadImage('https://static.wikia.nocookie.net/survivio/images/f/f1/Gun-med-01.img.png/revision/latest/scale-to-width-down/32?cb=20190830235718');
+  forestcamobase = loadImage('https://static.wikia.nocookie.net/survivio/images/2/27/Player-base-outfitMilitary.png/revision/latest/scale-to-width-down/141?cb=20200705015400');
+  snowcamobase = loadImage('https://static.wikia.nocookie.net/survivio/images/1/10/Player-base-outfitSpetsnaz.png/revision/latest/scale-to-width-down/140?cb=20200705015519');
+  urbancamobase = loadImage('https://static.wikia.nocookie.net/survivio/images/9/9b/Player-base-outfitUrbanCamo.png/revision/latest/scale-to-width-down/141?cb=20200705020024');
+  desertcamobase = loadImage('https://static.wikia.nocookie.net/survivio/images/5/5a/Player-base-outfitWheat.png/revision/latest/scale-to-width-down/140?cb=20200705020032');
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -33,40 +39,36 @@ function draw() {
   background(0);
   imageMode(CENTER);
   image(backgimg, width / 2, height / 2, width * 1.6, width / 1);
-  image(logo, width / 2, height / 5, width / 3, width / 12);
-  image(menurect, width / 1.95, height / 1.4, width / 1.5, height / 1.5);
+  image(logo, width / 2, height / 5, width / 3, width / 7);
+  image(menurect, width / 1.95, height / 1.4, height / 1.2, height / 1.5);
   image(battleicon, width / 3.2, height / 1.6, (height + width) / 20, (height + width) / 20);
   image(menugun, width / 2, height / 1.6, (height + width) / 20, (height + width) / 20);
   image(survivr1, width / 1.5, height / 1.6, (height + width) / 20, (height + width) / 20);
-  textSize(width / 16);
   textAlign(CENTER);
-  fill(255);
-  textFont(jura);
-  text('5v5', width / 2, height / 2.8);
   textFont(roboto);
   rectMode(CENTER);
-  fill(131, 150, 80);
+  fill(131, 140, 80);
   rect(width / 3.2, height / 1.32 + 3, width / 9, width / 25, 5);
   fill(131, 175, 80);
   if(mouseX >= width / 3.2 - width / 18 && mouseX <= width / 3.2 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
     cursor('not-allowed');
-    fill(131, 150, 80);
+    fill(131, 140, 80);
   }
   rect(width / 3.2, height / 1.32, width / 9, width / 25, 5);
-  fill(131, 150, 80);
+  fill(131, 140, 80);
   rect(width / 2, height / 1.32 + 3, width / 9, width / 25, 5);
   fill(131, 175, 80);
   if(mouseX >= width / 2 - width / 18 && mouseX <= width / 2 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
     cursor('not-allowed');
-    fill(131, 150, 80);
+    fill(131, 140, 80);
   }
   rect(width / 2, height / 1.32, width / 9, width / 25, 5);
-  fill(131, 150, 80);
+  fill(131, 140, 80);
   rect(width / 1.5, height / 1.32 + 3, width / 9, width / 25, 5);
   fill(131, 175, 80);
   if(mouseX >= width / 1.5 - width / 18 && mouseX <= width / 1.5 + width / 18 && mouseY >= height / 1.32 - width / 50 && mouseY <= height / 1.32 + width / 50) {
-    cursor('not-allowed');
-    fill(131, 150, 80);
+    cursor('pointer');
+    fill(131, 140, 80);
   }
   rect(width / 1.5, height / 1.32, width / 9, width / 25, 5);
   textSize(width / 40);
